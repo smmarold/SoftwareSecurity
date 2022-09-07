ab3d2026aaaf6450ca1bf904df486e069b02e105
smmarold <stephen.marold@gmail.com>
2022-09-07 18:31:36 -0400

    login try catch

--- model/model_login.php
+++ model/model_login.php
@@ -4,25 +4,29 @@ include (__DIR__ . '/db.php');
 
 //Function to log user in
 function login($userName, $userPassword){
+    try{
     //Global variable for database
-    global $db;
+        global $db;
 
-    $result = ["userID"=>"", "accountType"=>""];
+        $result = ["userID"=>"", "accountType"=>""];
 
-    //Create PDO statement object
-    $stmt = $db->prepare("SELECT userID, accountType FROM Users_Lookup WHERE userName=:userName AND userPassword=:userPassword");
+        //Create PDO statement object
+        $stmt = $db->prepare("SELECT userID, accountType FROM Users_Lookup WHERE userName=:userName AND userPassword=:userPassword");
 
-    //Store SQL statement argumants
-    $stmt->bindValue(':userName', $userName);
-    $stmt->bindValue(':userPassword', sha1("LRSE-salt" . $userPassword));
+        //Store SQL statement argumants
+        $stmt->bindValue(':userName', $userName);
+        $stmt->bindValue(':userPassword', sha1("LRSE-salt" . $userPassword));
 
-    //Check if PDO statement was executed and there is more then 1 row in table
-    if ($stmt->execute() && $stmt->rowCount() > 0 ) {
-        //Get query results
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);           
+        //Check if PDO statement was executed and there is more then 1 row in table
+        if ($stmt->execute() && $stmt->rowCount() > 0 ) {
+            //Get query results
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);           
+        }
+
+        //Return user record if one is found
+        return($result);
+    } catch (Exception $e) {
+        header("Location: errorPage.php");
     }
-
-    //Return user record if one is found
-    return($result);
 }
 ?>
